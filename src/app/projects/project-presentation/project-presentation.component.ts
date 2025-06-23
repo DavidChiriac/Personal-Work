b3ac16ee3930bddcab440058da7e18a92ba30234
CrcDavid <118164428+CrcDavid@users.noreply.github.com>
2025-06-23 19:46:05 +0300

    Make project presentation responsive

--- src/app/projects/project-presentation/project-presentation.component.ts
+++ src/app/projects/project-presentation/project-presentation.component.ts
@@ -3,20 +3,27 @@ import { IProject } from '../../shared/interfaces/project.interface';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectsService } from '../../shared/services/projects.service';
 import { ImageModule } from 'primeng/image';
+import { CommonModule } from '@angular/common';
+import { DeviceService } from '../../shared/services/device.service';
 
 @Component({
   selector: 'app-project-presentation',
-  imports: [ImageModule],
+  imports: [CommonModule, ImageModule],
   templateUrl: './project-presentation.component.html',
   styleUrl: './project-presentation.component.scss',
 })
 export class ProjectPresentationComponent implements OnInit {
   project!: IProject;
 
+  isMobile!: boolean;
+
   constructor(
-    private route: ActivatedRoute,
-    private readonly projectsService: ProjectsService
-  ) {}
+    private readonly route: ActivatedRoute,
+    private readonly projectsService: ProjectsService,
+    private readonly deviceService: DeviceService
+  ) {
+    this.isMobile = deviceService.isMobile();
+  }
 
   ngOnInit(): void {
     this.project = this.projectsService.getSingleProject(
